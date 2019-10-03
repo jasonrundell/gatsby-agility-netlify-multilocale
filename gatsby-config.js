238fd692bc5243f01c53f9714b7c4fea0478f1ee
Jason Rundell <jason.rundell@gmail.com>
2019-10-03 10:44:37 -0400

    Changing to en-us for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
         guid: agilityConfig.guid, //your Agility Content Fetch API Guid
         apiKey: agilityConfig.accessToken, //your Agility Content Fetch API Key
         sharedContent: ['posts', 'globalheader'], //a list of reference names you want to include in your GraphQL store
-        languages: ['en-ca', 'fr-ca'], //the languages you want to include
+        languages: ['en-us', 'fr-ca'], //the languages you want to include
         channels: ['website'], //the channels you want to include
         defaultPageTemplate: './src/templates/AgilityPage.js', //the page template that will be used to render Agility CMS pages
         indexPage: '/home', //If you want an Agility CMS page to be your home page (i.e. '/home' to be used as '/'), set the page path here
